f012c6bde27625d8947853664b338384131f7463
BHASKARAN RATHNACHALAM <bhaskaran@BHASKARANs-Mac-mini.local>
2018-12-20 10:47:36 -0500

    migration fixes

--- services/postgres/migrate.sh
+++ services/postgres/migrate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function up() {
-    migrations_string=$(find ./database/migrations -type f -not -path "*truncate*" | sort)
+    migrations_string=$(find ./migrations -type f -not -path "*truncate*" | sort)
     IFS=$'\n' read -rd '' -a migrations <<< "${migrations_string}"
     schema_name="$1"
     mkdir -p dist
