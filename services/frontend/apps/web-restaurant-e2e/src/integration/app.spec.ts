720bc040cc6701758e503bdb2bbce183a1a9d407
BHASKARAN RATHNACHALAM <bhaskaran@BHASKARANs-Mac-mini.local>
2018-12-27 18:45:35 -0500

    rename file and other fixes for restaurant app

--- services/frontend/apps/web-restaurant-e2e/src/integration/app.spec.ts
+++ services/frontend/apps/web-restaurant-e2e/src/integration/app.spec.ts
@@ -6,6 +6,6 @@ describe("Hello Nx", () => {
   beforeEach(() => cy.visit("/"));
 
   it("should display welcome message", () => {
-    getGreeting().contains("Welcome to web-web-restaurant!");
+    getGreeting().contains("Welcome to web-restaurant!");
   });
 });
--- services/frontend/apps/web-restaurant/karma.conf.js
+++ services/frontend/apps/web-restaurant/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
     ...baseConfig,
     coverageIstanbulReporter: {
       ...baseConfig.coverageIstanbulReporter,
-      dir: join(__dirname, "../../coverage/apps/web-web-restaurant")
+      dir: join(__dirname, "../../coverage/apps/web-restaurant")
     }
   });
 };
