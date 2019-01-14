038064396a3127b2819eac45fd832e3bed3490bc
Shaun Zeng <zengxiangxiang@gmail.com>
2019-01-13 21:27:09 -0500

    merge

---
+++ services/frontend-new/libs/feature-restaurant-menu/src/lib/newmenu/newmenu.component.spec.ts
@@ -0,0 +1,36 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule, MatCardModule, MatInputModule, MatRadioModule, MatSelectModule } from "@angular/material";
+
+import { NewmenuComponent } from "./newmenu.component";
+
+describe("NewmenuComponent", () => {
+  let component: NewmenuComponent;
+  let fixture: ComponentFixture<NewmenuComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [NewmenuComponent],
+      imports: [
+        NoopAnimationsModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatCardModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewmenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should compile", () => {
+    expect(component).toBeTruthy();
+  });
+});
