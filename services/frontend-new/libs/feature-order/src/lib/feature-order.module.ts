36aa2237cb53b279dec21fb6767e2163c3e4d686
Bhaskaran <bhaskaran@Bhaskarans-Mac-mini-2.local>
2019-01-20 17:43:29 -0500

    remove oder

--- services/frontend-new/libs/feature-order/src/lib/feature-order.module.ts
+++ services/frontend-new/libs/feature-order/src/lib/feature-order.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart/cart.component';
-import { OrderhistoryComponent } from './orderhistory/orderhistory.component';
 import { OrderHistoryComponent } from './order-history/order-history.component';
 import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 
 @NgModule({
   imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
-  declarations: [CartComponent, OrderhistoryComponent, OrderHistoryComponent]
+  declarations: [CartComponent,  OrderHistoryComponent]
 })
 export class FeatureOrderModule {}
