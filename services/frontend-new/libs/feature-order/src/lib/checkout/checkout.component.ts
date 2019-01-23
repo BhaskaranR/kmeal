b86ff5bcdc045db15ee31873aaf0e730c53e36ad
Shaun Zeng <zengxiangxiang@gmail.com>
2019-01-22 21:35:43 -0500

    checkout

--- services/frontend-new/libs/feature-order/src/lib/checkout/checkout.component.ts
+++ services/frontend-new/libs/feature-order/src/lib/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { MatDialog } from "@angular/material";
+import { DishOrderComponent } from "libs/ui/src/lib/dish-order/dish-order.component";
 
 
 @Component({
@@ -6,9 +8,53 @@ import { Component, OnInit, ViewChild } from "@angular/core";
   templateUrl: "./checkout.component.html",
 })
 export class CheckoutComponent implements OnInit {
-    constructor(){}
+    @Input() data:any;
+    subTotal:number ;
+    tax:number;
+    fee:number;
+    total:number
+    constructor(public dialog: MatDialog,){}
 
     ngOnInit(){
         console.log('checkout')
+    }
+
+    changeOrder(){
+        let data = {
+            name:'Chicken noodle',
+            id:'hxusa2432nk',
+            specifications:[
+                {
+                    type:'number',
+                    inputs:'',
+                    name:'Quantity',
+                    value:null,
+                },{
+                    type:'select',
+                    inputs:['Mild','Medium','Extra Spicy'],
+                    value:null,
+                    name:'Spicy Level'
+                },{
+                    type:'multiple',
+                    inputs:['extra shrimp','extra chicken breast','salad'],
+                    name:'AddOn',
+                    value:null,
+                },{
+                    type:'string',
+                    inputs:'',
+                    name:'Other Instructions',
+                    value:null
+                }
+            ]
+        }
+
+        const dialogRef = this.dialog.open(DishOrderComponent, {
+            width: '650px',
+            data: data
+          });
+      
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('closed', result);
+        });
     }
 }
--- services/frontend-new/libs/feature-order/src/lib/feature-order.module.ts
+++ services/frontend-new/libs/feature-order/src/lib/feature-order.module.ts
@@ -4,10 +4,11 @@ import { CartComponent } from './cart/cart.component';
 import { OrderHistoryComponent } from './order-history/order-history.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { FeatureCoreModule } from '@kmeal-nx/feature-core';
+import { UiModule } from '@kmeal-nx/ui';
 
 
 @NgModule({
-  imports: [CommonModule, FeatureCoreModule],
+  imports: [CommonModule, FeatureCoreModule, UiModule],
   declarations: [CartComponent, OrderHistoryComponent, CheckoutComponent]
 })
 export class FeatureOrderModule {}
