0cab13b97eae7226101c0e128a4052b221dc58fc
Shaun Zeng <zengxiangxiang@gmail.com>
2019-03-13 20:44:20 -0400

    profile page

--- services/frontend-new/libs/feature-profile/src/lib/feature-profile.module.ts
+++ services/frontend-new/libs/feature-profile/src/lib/feature-profile.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { AccountService } from './services/account.service';
 import { ScatterModule } from '@kmeal-nx/scatter';
+import { ProfileClientComponent } from './profile-clientApp/profile-client.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FeatureCoreModule } from '@kmeal-nx/feature-core';
 
 @NgModule({
-  imports: [CommonModule, ScatterModule],
-  declarations: [ProfileComponent],
+  imports: [CommonModule,FlexLayoutModule, ScatterModule, FeatureCoreModule],
+  declarations: [ProfileComponent, ProfileClientComponent],
+  exports:[ProfileComponent, ProfileClientComponent],
   providers:[AccountService]
 })
 export class FeatureProfileModule {}
