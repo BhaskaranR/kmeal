dcc681d373e946c7738b7a0cd59d1015a1543db4
Bhaskaran <bhaskaran@Bhaskarans-Mac-mini-2.local>
2019-05-12 18:23:47 -0400

    fixes

--- services/postgres/migrations/00071_views.sql
+++ services/postgres/migrations/00071_views.sql
@@ -1,35 +1,23 @@
--- select kmeal.get_nearby(40.710237, -74.007810, 4, 'italian', 'REGULAR');
+-- select kmeal.get_nearby(40.710237, -74.007810, 4);
 
 create table "kmeal"."restaurant_nearby" (
   "restaurant_id" INTEGER NOT NULL PRIMARY KEY REFERENCES  kmeal.restaurant("restaurant_id"),
-  "owner" CHAR(12) NOT NULL,
-  "name" TEXT NOT NULL,
-  "description" TEXT NOT NULL,
-  "phone"       VARCHAR(15) NOT NULL,
-  "latitude" FLOAT NOT NULL,
-  "longitude" FLOAT NOT NULL,
-  "location" GEOGRAPHY(Point),
-  "address"     TEXT NOT NULL,
-  "logo"        TEXT NOT NULL,
-  "rating"      INTEGER,
-  "timeofoperation" VARCHAR(60) NOT NULL,
-  "isactive"    BOOLEAN NOT NULL,
    "distance" DECIMAL NOT NULL 
   );
-CREATE FUNCTION  kmeal.get_nearby(latitude FLOAT, longitude FLOAT, radius INTEGER)  RETURNS SETOF kmeal.restaurant_nearby AS $$
+
+
+CREATE OR REPLACE FUNCTION  kmeal.get_nearby(latitude FLOAT, longitude FLOAT, radius INTEGER)  
+RETURNS SETOF kmeal.restaurant_nearby AS $$
      SELECT ROW(d.restaurant_id,
-            d.name as name,
-            d.owner as owner,
-            d.description as description,
-            d.phone as phone,
-            d.latitude as latitude,
-            d.longitude as longitude,
-            d.location as location,
-            d.address as address,
-            d.logo as logo,
-            d.rating as rating,
-            d.timeofoperation as timeofoperation,
-            d.isactive as isactive,
+            d.name,
+            d.owner,
+            d.description,
+            d.phone,
+            d.address,
+            d.logo,
+            d.rating,
+            d.timeofoperation,
+            d.isactive,
             d.distance)::kmeal.restaurant_nearby
     FROM (
     SELECT z.restaurant_id as restaurant_id,
@@ -41,7 +29,7 @@ CREATE FUNCTION  kmeal.get_nearby(latitude FLOAT, longitude FLOAT, radius INTEGE
             z.logo as logo,
             z.rating as rating,
             z.timeofoperation as timeofoperation,
-            z.location as location
+            z.location as location,
             z.isactive as isactive,
             z.latitude as latitude, 
             z.longitude as longitude,
@@ -65,7 +53,7 @@ CREATE FUNCTION  kmeal.get_nearby(latitude FLOAT, longitude FLOAT, radius INTEGE
             AND p.longpoint + (radius / (p.distance_unit * COS(RADIANS(p.latpoint))))
        -- AND  rc.category = cuisine AND z.timeofoperation = timeofop
     ) AS d
-    WHERE distance <= 4
-    ORDER BY distance
-    LIMIT 100;
+    WHERE distance <= 4;
+    -- ORDER BY distance
+    -- LIMIT 100;
 $$ LANGUAGE SQL STABLE;
