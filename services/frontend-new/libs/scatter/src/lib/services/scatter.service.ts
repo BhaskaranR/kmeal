64c1eda4f27f75e730da0eb42f70eecc6871a7ad
Bhaskaran <bhaskaran@Bhaskarans-Mac-mini-2.local>
2019-05-24 08:10:24 -0400

    scatter demux changes

--- services/frontend-new/libs/scatter/src/lib/services/scatter.service.ts
+++ services/frontend-new/libs/scatter/src/lib/services/scatter.service.ts
@@ -12,7 +12,7 @@ export let contract;
 
 import { HttpClient } from '@angular/common/http';
 import { BigNumber } from 'bignumber.js';
-export const code = 'kmealadmin15';
+export const code = 'kmealadminio';
 
 export const RETURN_TYPES = {
     ERROR: 0,
