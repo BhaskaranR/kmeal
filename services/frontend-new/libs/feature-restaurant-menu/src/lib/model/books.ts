0fea3b1a8ceaee066b8720d0c422e5219dc18436
Bhaskaran <bhaskaran@Bhaskarans-Mac-mini-2.local>
2019-06-02 16:57:06 -0400

    added is_active

--- services/frontend-new/libs/feature-restaurant-menu/src/lib/model/books.ts
+++ services/frontend-new/libs/feature-restaurant-menu/src/lib/model/books.ts
@@ -1,7 +1,7 @@
 export class Book {
     book_id: number;
     book_name: string;
-
+    is_active: boolean;
     static fromJson(json){ return Object.assign(Book.placeholder(), json); }
     static placeholder(){ return new Book(); }
 }
