c308d564071b0181120be41f8bb2510f174f35a9
Bhaskaran <bhaskaran@Bhaskarans-Mac-mini-2.local>
2019-07-23 18:53:51 -0400

    cleanup

--- contracts/kmeal/define.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-static constexpr  uint8_t   ORDERED      = 1;
-static constexpr  uint8_t   PAID        = 2;
-static constexpr  uint8_t   CANCELLED   = 3;
-static constexpr  uint8_t   PREPARING   = 4;
-static constexpr  uint8_t   PICKEDUP    = 5;
-static constexpr  uint8_t   DELIVERED    = 6;
